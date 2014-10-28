9761e23254a53baf05ec2c5a5870350820b48879
Hisand Liu <hisand.liu@gmail.com>
2014-10-28 09:48:28 +0800

    AAA

--- code/index.php
+++ code/index.php
@@ -5,15 +5,23 @@ date_default_timezone_set("Asia/Shanghai");
 require_once 'config.php';
 require_once 'org/hisand/db/BasicDAO.php';
 
+session_start();
 $bible_code = '';
 if (isset($_GET['p'])) {
 	$_SESSION['bible_code'] = $_GET['p'];
+	
 	$bible_code = $_GET['p']; 
+	$_COOKIE['bible_code'] = $_GET['p'];
 }
 else {
 	$_SESSION['bible_code'] = 'index';
 	$bible_code = 'index';
+	$_COOKIE['bible_code'] = $_GET['p'];
 }
+
+$value = 'something from somewhere';
+
+setcookie("TestCookie", $value);
 
 if ('index' == $bible_code) {
 	require_once 'home.php';
